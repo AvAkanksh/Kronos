1c47912660e11ad70522f025f6c3d16bd93b2bff
AvAkanksh <ee18b002@iittp.ac.in>
2025-10-07 12:00:36 +0530

    added technical docs

--- examples/prediction_example.py
+++ examples/prediction_example.py
@@ -43,14 +43,14 @@ tokenizer = KronosTokenizer.from_pretrained("NeoQuasar/Kronos-Tokenizer-base")
 model = Kronos.from_pretrained("NeoQuasar/Kronos-small")
 
 # 2. Instantiate Predictor
-predictor = KronosPredictor(model, tokenizer, device="cuda:0", max_context=512)
+predictor = KronosPredictor(model, tokenizer, device="cuda:0", max_context=1000)
 
 # 3. Prepare Data
 df = pd.read_csv("./data/XSHG_5min_600977.csv")
 df['timestamps'] = pd.to_datetime(df['timestamps'])
 
 lookback = 400
-pred_len = 120
+pred_len = 1000
 
 x_df = df.loc[:lookback-1, ['open', 'high', 'low', 'close', 'volume', 'amount']]
 x_timestamp = df.loc[:lookback-1, 'timestamps']
@@ -71,6 +71,8 @@ pred_df = predictor.predict(
 # 5. Visualize Results
 print("Forecasted Data Head:")
 print(pred_df.head())
+print(pred_df)
+print(len(pred_df))
 
 # Combine historical and forecasted data for plotting
 kline_df = df.loc[:lookback+pred_len-1]
